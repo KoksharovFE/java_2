c70e0dcf711358e698252268e167a7ebab5b9fe7
KoksharovFE <konnor2007@gmail.com>
2014-10-20 01:46:38 +0400

    Auth_begin
    Auth view  + begin of class

--- m_fact/src/main/java/com/astra/app/factograph/m_fact/Auth.java
+++ m_fact/src/main/java/com/astra/app/factograph/m_fact/Auth.java
@@ -2,46 +2,46 @@ package com.astra.app.factograph.m_fact;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
 
-public class MainActivity extends Activity {
+public class Auth extends Activity {
+    private UserDbAdapter dbHelper;
+    private static final int ACTIVITY_CREATE = 0;
+    private static final int ACTIVITY_EDIT = 1;
+    private static final int DELETE_ID = Menu.FIRST + 1;
+    private Cursor cursor;
 
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        setContentView(R.layout.activity_auth);
+        dbHelper = new UserDbAdapter(this);
+        dbHelper.open();
     }
 
     public void buttonClicked(View view) {
         switch (view.getId()) {
             case R.id.button: {
-                Intent intent = new Intent(MainActivity.this, Overview.class);
-                startActivity(intent);
-            }
-            case R.id.button2: {
-                this.finish();
-
-            }
-            case R.id.button3: {
-                Intent intent = new Intent(MainActivity.this, Auth.class);
-                startActivity(intent);
+                cursor = dbHelper.fetchAllTodos();
+                String[] from = new String[]{UserDbAdapter.KEY_LOGIN};
+                for(int i=0;i<from.length;i++) {
+                    if (from[i]=="true") {
+                        Intent intent = new Intent(Auth.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
             }
 
         }
-
     }
 
 
@@ -57,5 +57,4 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
---
+++ m_fact/src/main/java/com/astra/app/factograph/m_fact/UserDbUsing.java
@@ -0,0 +1,41 @@
+package com.astra.app.factograph.m_fact;
+
+/**
+ * Created by teodor on 19.10.2014.
+ */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class UserDbUsing extends SQLiteOpenHelper{
+    private static final String DATABASE_NAME = "userdata";
+    private static final int DATABASE_VERSION = 1;
+    // запрос на создание базы данных
+    private static final String DATABASE_CREATE = "create table users (_id integer primary key autoincrement, "
+            + "login text not null, password text not null, rights text not null);";
+
+    public UserDbUsing(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // метод вызывается при создании базы данных
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    // метод вызывается при обновлении базы данных, например, когда вы увеличиваете номер версии базы данных
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion,
+                          int newVersion) {
+        Log.w(DatabaseUsing.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data"
+        );
+        database.execSQL("DROP TABLE IF EXISTS usersData");
+        onCreate(database);
+
+    }
+}
+

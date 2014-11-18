3db85beef6a653121ce56e89058dd0ddfc2e92a7
KoksharovFE <konnor2007@gmail.com>
2014-11-18 14:01:47 +0300

    0.3.19
    links custom_spinner begins

--- m_fact/src/main/java/com/astra/app/factograph/m_fact/Links.java
+++ m_fact/src/main/java/com/astra/app/factograph/m_fact/Links.java
@@ -2,15 +2,22 @@ package com.astra.app.factograph.m_fact;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class Links extends Activity {
@@ -159,6 +166,41 @@ public class Links extends Activity {
         }
         // getters and setters...
     }
+
+    private class MyEDListViewAdapter extends ArrayAdapter<Item> {
+
+        private final Context context;
+        private final ArrayList<Item> itemsArrayList;
+
+        public MyEDListViewAdapter(Context context, ArrayList<Item> itemsArrayList) {
+            super(context, R.layout.row, itemsArrayList);
+            this.context = context;
+            this.itemsArrayList = itemsArrayList;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            // 1. Create inflater
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            // 2. Get rowView from inflater
+            View rowView = inflater.inflate(R.layout.row, parent, false);
+
+            // 3. Get the two text view from the rowView
+            TextView labelView = (TextView) rowView.findViewById(R.id.spinner_text);
+            TextView descriptionView = (TextView) rowView.findViewById(R.id.spinner_description);
+
+            // 4. Set the text for textView
+            labelView.setText(itemsArrayList.get(position).getTitle());
+            descriptionView.setText(itemsArrayList.get(position).getDescription());
+
+            // 5. retrn rowView
+            return rowView;
+        }
+
+    };
 
 
     @Override
